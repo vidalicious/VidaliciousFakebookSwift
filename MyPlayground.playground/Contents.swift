3247376a5b6e513ba8ea7ef701b7814ca82ae1c1
vidalicious <vida.gu@cootek.cn>
2019-05-29 11:48:12 +0800

    dev

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,18 @@
 import UIKit
 import PlaygroundSupport
+import Dispatch
 
+let view1 = UIView(frame: CGRect(x: 0, y: 200, width: 199, height:500))
+view1.backgroundColor = UIColor.red
 
+PlaygroundPage.current.liveView=view1
 
+class Test {
+    var name : String {
+        return ""
+    }
+    
+    var age : Int = {
+       return 10
+    }()
+}
